cdd3245feed452b6fa3499b55df7278c4c318c92
flightlogscan <flightlogscan@flightlogscan.com>
2025-06-21 08:39:28 -0700

    initial swift

---
+++ Scramble/Scramble/ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambleApp.swift
+//  Scramble
+//
+//  Created by Wilbur 😎 on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Scramble/ScrambleTests/ScrambleTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScrambleTests.swift
+//  ScrambleTests
+//
+//  Created by Wilbur 😎 on 6/21/25.
+//
+
+import Testing
+
+struct ScrambleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
